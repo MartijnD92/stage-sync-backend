5a041f891a6a0fe4848b28853702eeba43d90e8b
Martijn Doensen <martijn.doensen@live.nl>
2021-04-16 09:45:21 +0200

    Add all backend files to separate repo

---
+++ stage-sync-backend/src/main/java/nl/stagesync/stagesync/exception/NotAuthorizedException.java
@@ -0,0 +1,12 @@
+package nl.stagesync.stagesync.exception;
+
+public class NotAuthorizedException extends RuntimeException{
+
+    public NotAuthorizedException() {
+        super();
+    }
+
+    public NotAuthorizedException(String message) {
+        super(message);
+    }
+}
