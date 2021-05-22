48154c23a5794eb11f33ce470809bb432a771fac
Martijn Doensen <martijn.doensen@live.nl>
2021-05-22 09:44:38 +0200

    Remove role assignment on signup and insert admin user in data.sql

--- stage-sync-backend/src/main/resources/data.sql
+++ stage-sync-backend/src/main/resources/data.sql
@@ -3,6 +3,11 @@ INSERT INTO role(name) VALUES('ROLE_USER');
 INSERT INTO role(name) VALUES('ROLE_MODERATOR');
 INSERT INTO role(name) VALUES('ROLE_ADMIN');
 
+INSERT INTO app_user(first_name, last_name, username, email, password)
+VALUES('admin', 'admin', 'admin', 'admin@stagesync.nl', '$2a$10$ExlSiTek2JcjonpJp4Ujg.gj8O9pdCLPS09jsHAp2XT1VX2H1/kt.');
+INSERT INTO user_role(user_id, role_id) VALUES(1, 1);
+INSERT INTO user_role(user_id, role_id) VALUES(1, 3);
+
 -- INSERT INTO artist (name, price, has_sound_engineer, genre) VALUES ('The Hillbilly Moonshiners', 1500, true, 'Bluegrass');
 -- INSERT INTO artist (name, price, has_sound_engineer, genre) VALUES ('The Beatles', 5000000, true, 'Pop');
 
